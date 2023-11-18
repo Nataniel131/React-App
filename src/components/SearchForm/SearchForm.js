8a20493128486a93daf585efc4e566ae380ee8e1
Nataniel Rutkowski <rutkowski.nataniel@gmail.com>
2023-11-18 20:46:07 +0100

    Add columns and cards

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,5 +1,5 @@
-import styles from './SearchForm.module.scss'
-import TextInput from '../TextInput/TextInput'
+import styles from './SearchForm.module.scss';
+import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 
 const SearchForm = () => {
