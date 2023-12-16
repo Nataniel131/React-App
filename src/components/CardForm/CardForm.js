46ea7144ae4bc698a1663467646d4bd44f3c6ff8
Nataniel Rutkowski <rutkowski.nataniel@gmail.com>
2023-12-16 15:52:32 +0100

    Add actions for reducer in store.js

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,13 +2,16 @@ import styles from './CardForm.module.scss';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const CardForm = props => {
   const [title, setTitle] = useState('');
+  const columnId = props.columnId;
+  const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
-    props.action({ title: title }, props.columnId);
+    dispatch({ type: 'ADD_CARD', payload: { columnId, title} });
     setTitle('');
   }
 
